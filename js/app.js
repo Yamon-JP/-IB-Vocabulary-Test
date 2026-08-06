a9ebe6af761bfa7b43594b234cf60fbc9bd42bf1
Yamon-JP <takashiyamamoto.81@gmail.com>
2026-08-06 15:35:18 +0900

    Commit 003: Add application state and navigation foundation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
 const App = {
+  state: {
+    currentPage: 'home',
+    subject: null,
+    user: {
+      xp: 0,
+      streak: 0
+    }
+  },
+
   init() {
+    this.loadState();
     console.log('IB Master Trainer Version 1.0 initialized');
+    UI.render(`Current Page: ${this.state.currentPage}`);
+  },
+
+  navigate(page) {
+    this.state.currentPage = page;
+    this.saveState();
+    UI.render(`Navigate: ${page}`);
+  },
+
+  loadState() {
+    const saved = Storage.load('ib_master_trainer_state');
+    if (saved) {
+      this.state = saved;
+    }
+  },
+
+  saveState() {
+    Storage.save('ib_master_trainer_state', this.state);
   }
 };
 
